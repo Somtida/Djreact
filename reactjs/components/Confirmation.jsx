2b5bf439de194d1f46ecd46394aaf52f6aa4e8b6
S.SKNS <somtida.th@gmail.com>
2016-09-21 22:51:30 -0700

    confirmation page

---
+++ reactjs/components/Confirmation.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class Confirmation extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <p>Register Successful</p>
+      </div>
+    )
+  }
+}
